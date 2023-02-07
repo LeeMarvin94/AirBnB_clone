232f2db854735a27b3341afa1a0c1b1a777ef042
LeeMarvin94 <rollymarvinondoabaghe@gmail.com>
2023-02-07 18:10:42 +0100

    First full implmentation of base model task

--- models/base_model.py
+++ models/base_model.py
@@ -2,14 +2,27 @@
 """ python code that implements the base model of a Airbnb clone + } =  """
 from uuid import uuid4
 from datetime import datetime
+from models import storage
 
 class BaseModel:
     """ represents the base model class of the Airbnb project """
     def __init__(self,*args,**kwargs):
         """ initializes a instance attributes """
-        self.id = str(uuid4())    # the goal of using the module uuid4 is to have a unique id for each Base module object
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid4())    # the goal of using the module uuid4 is to have a unique id for each Base module object
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """ prints a string representation of a object """
@@ -19,9 +32,15 @@ class BaseModel:
     def save(self):
         """ updates the public instance attribute with the current datetime """
         self.updated_at = datetime.now()
+        storage.save()
         
     def to_dict(self):
         """ returns a dictionary containing all keys/values of the instance """
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = type(self).__name__
+        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        return my_dict
         
         
         
