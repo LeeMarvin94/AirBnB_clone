c56ceba2f7af2eee70225f6547bb4d6df2d97dd2
LeeMarvin94 <rollymarvinondoabaghe@gmail.com>
2023-02-20 14:40:25 +0100

    edit

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module for FileStorage class"""
+"""Module for FileStorage class."""
 import datetime
 import json
 import os
@@ -31,8 +31,20 @@ class FileStorage:
     def classes(self):
         """Returns a dictionary of valid classes and their references."""
         from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
 
-        classes = {"BaseModel": BaseModel}
+        classes = {"BaseModel": BaseModel,
+                   "User": User,
+                   "State": State,
+                   "City": City,
+                   "Amenity": Amenity,
+                   "Place": Place,
+                   "Review": Review}
         return classes
 
     def reload(self):
@@ -52,6 +64,34 @@ class FileStorage:
             "BaseModel":
                      {"id": str,
                       "created_at": datetime.datetime,
-                      "updated_at": datetime.datetime}
+                      "updated_at": datetime.datetime},
+            "User":
+                     {"email": str,
+                      "password": str,
+                      "first_name": str,
+                      "last_name": str},
+            "State":
+                     {"name": str},
+            "City":
+                     {"state_id": str,
+                      "name": str},
+            "Amenity":
+                     {"name": str},
+            "Place":
+                     {"city_id": str,
+                      "user_id": str,
+                      "name": str,
+                      "description": str,
+                      "number_rooms": int,
+                      "number_bathrooms": int,
+                      "max_guest": int,
+                      "price_by_night": int,
+                      "latitude": float,
+                      "longitude": float,
+                      "amenity_ids": list},
+            "Review":
+            {"place_id": str,
+                         "user_id": str,
+                         "text": str}
         }
         return attributes
